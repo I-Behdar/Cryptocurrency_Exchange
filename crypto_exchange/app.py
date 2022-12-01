0ff615097edd11cae48769d7c1c7d5034aa46ae0
puyan <p.behdarvandi@gmail.com>
2022-12-01 23:55:23 +0100

    Exchange cryptocurrency to federal currency

---
+++ crypto_exchange/app.py
@@ -0,0 +1,48 @@
+import argparse
+import requests
+
+
+def parser():
+    parser = argparse.ArgumentParser(description='Exchange crypto currency to a given currency')
+
+    parser.add_argument('amount', type=float,
+                        help='a float number for the crypto currency')
+    parser.add_argument('crypto_currency', type=str,
+                        help='sum the integers (default: find the max)')
+    parser.add_argument('currency', type=str,
+                        help='sum the integers (default: find the max)')
+    args = parser.parse_args()
+    return args
+
+
+data = parser()
+
+
+def fetch_symbol(crypto: str) -> str:
+    r = requests.get('https://api.coingecko.com/api/v3/coins/list')
+    symbols = r.json()
+
+    for i in range(len(symbols)):
+        if symbols[i]['id'] == crypto:
+            return symbols[i]['symbol']
+
+
+def fetch_rates(crypto: str, currency: str) -> float:
+    r = requests.get(f"https://api.coingecko.com/api/v3/coins/markets?vs_currency={currency}")
+
+    rate = r.json()
+
+    for i in range(len(rate)):
+        if rate[i]['id'] == crypto:
+            return rate[i]['current_price']
+
+
+def conversion(amount: float, crypto_currency: str, normal_currency: str) -> float:
+    exchange_rate = fetch_rates(crypto_currency, normal_currency)
+    return round(amount * exchange_rate, 2)
+
+
+input = (data.amount, data.crypto_currency, data.currency)
+output = conversion(amount=input[0], crypto_currency=input[1], normal_currency=input[2])
+
+print(f"{fetch_symbol(data.crypto_currency)} {data.amount} = USD {output}")
